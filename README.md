e73576cbf130d34b39986ac034c052fcdb2b24c7
Scott Smeester <scottsmeester@elementss-MacBook-Pro-3.local>
2016-01-04 07:33:43 -0700

    getting access to data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #h4 Run the application by typing:
 `meteor run --settings server/settings.json`
+
+###Purpose
+This package allows the Meteor developer to easily create api connections to  and GET, POST, DELETE via api's of a huge amount of CRMs, document storage services, etc. api's via Cloud Elements.
--- meteor-login-pkg.js
+++ meteor-login-pkg.js
@@ -1,37 +1,63 @@
 // start the application by using the following command
 // meteor run --settings server/settings.json
+Contacts = {}
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Meteor.subscribe("contacts");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Meteor.call("ceContacts", function(error, results) {
+      console.log(results); //results.data should be a JSON object
   });
 }
 
 if (Meteor.isServer) {
+
+  Meteor.publish("contacts", function () {
+    return Contacts.find();
+  });
+
   Meteor.startup(function () {
-      // code to run on server at startup
-      // Accounts.loginServiceConfiguration.remove({
-      //   service: 'cloud-elements'
-      // });
 
-      // Accounts.loginServiceConfiguration.insert({
-      //   service: 'cloud-elements',
-      //   clientId: Meteor.settings.secret_stuff.CE_ORG_SECRET,
-      //   secret: Meteor.settings.secret_stuff.CE_USER_SECRET
-      // });
+    Accounts.loginServiceConfiguration.remove({
+      service: 'cloudelements'
+    });
 
-      console.log('Meteor.settings.secret_stuff.CE_ORG_SECRET', Meteor.settings.secret_stuff.CE_ORG_SECRET);
+    Accounts.loginServiceConfiguration.insert({
+      service: 'cloudelements',
+      org_secret: Meteor.settings.secret_stuff.CE_ORG_SECRET,
+      user_secret: Meteor.settings.secret_stuff.CE_USER_SECRET
+    });
+  });
+
+
+  Meteor.methods({
+      ceContacts: function () {
+        // var config = Accounts.loginServiceConfiguration.findOne({service: 'cloudelements'});
+        // console.log('config', config);
+        // if (!config) {
+        //     throw new ServiceConfiguration.ConfigError("Service not configured");
+        //   // this.unblock();
+        //   // return Meteor.http.call("GET", "https://console.cloud-elements.com:443/elements/api-v2/hubs/finance/customers");
+        // }
+        var responseContent;
+
+        try {
+            // Request an access token
+            responseContent = HTTP.get(
+                "https://console.cloud-elements.com/elements/api-v2/hubs/crm/accounts", {
+                    headers: {
+                        Authorization:  'User ' + Meteor.settings.secret_stuff.CE_USER_SECRET + ', Organization ' + Meteor.settings.secret_stuff.CE_ORG_SECRET + ', Element GyiNJi1HQFNZFAy/bTJA6/Psd9wbXXgNXit6qvloVxc'
+                    }
+                }).content;
+
+        } catch (err) {
+            throw _.extend(new Error("Failed to complete OAuth handshake with stripe. " + err.message),
+                {response: err.response});
+        }
+
+        console.log('responseContent', responseContent)
+    }
   });
 }
+
